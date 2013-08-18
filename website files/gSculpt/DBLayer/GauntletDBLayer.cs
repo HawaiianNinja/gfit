b46d99808d84c267fd1ab86e8d2acbb2f7adaf11
John Patterson <john.patterson@outlook.com>
2013-08-17 18:43:27 -0700

    Revert "No Change"
    This reverts commit 8335833d05aa007653b1301990b6eede4316910f.

--- website files/gSculpt/DBLayer/GauntletDBLayer.cs
+++ website files/gSculpt/DBLayer/GauntletDBLayer.cs
@@ -89,7 +89,6 @@ namespace gSculpt.DBLayer
             for (int i = 0; i < dt.Rows.Count; i++)
             {
 
-
                 Gauntlet g = new Gauntlet
                 {
                     Id = (int)GetColValue(dt.Rows[i], "id"),
