d615f6b0cc20bdf317bf766bb6bbcb9dfd41e5f3
HawaiianNinja <hopkinaj@rose-hulman.edu>
2013-08-19 01:00:27 -0700

    Fixed cache issue in webview

--- android app/gfit/src/com/gauntletfitness/gfit/MainActivity.java
+++ android app/gfit/src/com/gauntletfitness/gfit/MainActivity.java
@@ -2,6 +2,7 @@ package com.gauntletfitness.gfit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -9,23 +10,45 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        WebView view = (WebView)findViewById(R.id.webview);
-        view.setWebViewClient(new WebViewClient());
-        WebSettings webSettings = view.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        view.loadUrl("http://gfit.azurewebsites.net/");
-        //view.loadUrl("http://google.com/");
-    }
+	private WebView view;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		view = (WebView) findViewById(R.id.webview);
+		if (savedInstanceState != null) {
+			view.restoreState(savedInstanceState);
+		} else {
+			view.setWebViewClient(new WebViewClient());
+			view.clearCache(true);
+			view.loadUrl("http://gfit.azurewebsites.net/");
+		}
+		WebSettings webSettings = view.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Check if the key event was the Back button and if there's history
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+			view.goBack();
+			return true;
+		}
+		// If it wasn't the Back key or there's no web page history, bubble up
+		// to the default
+		// system behavior (probably exit the activity)
+		return super.onKeyDown(keyCode, event);
+	}
+
+	protected void onSaveInstanceState(Bundle outState) {
+		view.saveState(outState);
+	}
+
 }
